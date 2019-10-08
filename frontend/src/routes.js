21e19ad115277f3c1f754d3dd88ffa1f5707c10d
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-10-07 22:11:30 -0300

    Renaming pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,8 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import ExampleProfile from './pages/ExampleProfile';
 import ExamplesList from './pages/ExamplesList';
-import termsAndConditions from './pages/termsAndConditions';
-import Sobre from './pages/Sobre';
+import TermsAndConditions from './pages/TermsAndConditions';
+import AboutUs from './pages/AboutUs';
 
 export default function Routes() {
     return (
@@ -13,8 +13,8 @@ export default function Routes() {
             <Route path="/" exact component={ Homepage }/> {/* sem o exact, o router não irá diferenciar essas duas Routes, pois o Router compara o começo da rota somente*/ }
             <Route path="/exemplo/:id" component={ ExampleProfile }/>
             <Route path="/exemplos" exact component={ ExamplesList }/>
-            <Route path="/termos-e-condicoes" exact component={ termsAndConditions }/>
-            <Route path="/sobre" exact component={ Sobre }/>
+            <Route path="/termos-e-condicoes" exact component={ TermsAndConditions }/>
+            <Route path="/sobre" exact component={ AboutUs }/>
         </BrowserRouter>
     );
 }
