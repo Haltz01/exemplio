f0a5eeede4f718b65cb4695fab3fa4a65ee9acf4
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-10-25 11:16:12 -0300

    Adding 404 and fixing broken F5

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('/teste123/static', express.static(path.join(__dirname, 'build', 'static')));
+app.use('*/static', express.static(path.join(__dirname, 'build', 'static')));
 
 app.get(['/teste123','/teste123/*'], function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { linkPrefix } from './options.json';
 
 import Homepage from './pages/Homepage/Homepage';
@@ -7,15 +7,19 @@ import ExampleProfile from './pages/ExampleProfile/ExampleProfile.js';
 import ExamplesList from './pages/ExamplesList/ExamplesList';
 import TermsAndConditions from './pages/TermsAndConditions';
 import AboutUs from './pages/AboutUs';
+import NotFound from './pages/NotFound.js';
 
 export default function Routes() {
     return (
         <BrowserRouter>
-            <Route path={`/${linkPrefix}/`} exact component={ Homepage }/> {/* sem o exact, o router não irá diferenciar essa das outras Routes, pois o Router compara o começo da rota somente (startsWith)*/ }
-            <Route path={`/${linkPrefix}/exemplo/:exampleID`} component={ ExampleProfile }/>
-            <Route path={`/${linkPrefix}/exemplos/`} exact component={ ExamplesList }/>
-            <Route path={`/${linkPrefix}/termos-e-condicoes/`} exact component={ TermsAndConditions }/>
-            <Route path={`/${linkPrefix}/sobre/`} exact component={ AboutUs }/>
+            <Switch>
+                <Route path={`/${linkPrefix}`} exact component={ Homepage }/> {/* sem o exact, o router não irá diferenciar essa das outras Routes, pois o Router compara o começo da rota somente (startsWith)*/ }
+                <Route path={`/${linkPrefix}/exemplo/:exampleID`} component={ ExampleProfile }/>
+                <Route path={`/${linkPrefix}/exemplos/`} exact component={ ExamplesList }/>
+                <Route path={`/${linkPrefix}/termos-e-condicoes/`} exact component={ TermsAndConditions }/>
+                <Route path={`/${linkPrefix}/sobre/`} exact component={ AboutUs }/>
+                <Route path={`/${linkPrefix}/`} component={ NotFound }/>
+            </Switch>
         </BrowserRouter>
     );
 }
