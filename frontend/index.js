bbad9e4b08fd42f5dbc2065ba9003609f57d87bd
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-10-31 01:08:48 -0300

    Changing stuff

--- frontend/index.js
+++ frontend/index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const exec = require('child_process').exec;
+
+exec('cd build && git pull');
+
 const prefix = '/revamp'
 
 app.use('*static', express.static(path.join(__dirname, 'build', 'static')));
 
+app.get(`${prefix}/favicon.png`, function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'favicon.png'));
+});
 
 app.get(`${prefix}/*`, function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
