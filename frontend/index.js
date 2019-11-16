2815b48238e4da872f00aa362ae891b684d3fe31
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-11-16 14:10:14 -0300

    Production adjusts and attempt to fix IPhone Bug

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
 
 const exec = require('child_process').exec;
 
+exec('rm -rf /home/exemvoox/exempliorevamp/build')
 exec('git clone https://github.com/marcuscastelo/exemplio-build-frontend build');
 
-const prefix = '/revamp';
+const prefix = '';
 
 app.use('*static', express.static(path.join(__dirname, 'build', 'static')));
+
+app.get('/fallback', (req, res) => {
+	res.status(301).redirect('/fallback/');
+})
+
+app.get('*/manifest.json', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'manifest.json'))
+});
 
 app.get(`${prefix}/favicon.png`, function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'favicon.png'));
 });
 
 app.get(`${prefix}/*`, function(req, res) {
+  while(! fs.existsSync(path.join(__dirname, 'build', 'index.html')));
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
