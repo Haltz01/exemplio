58dfe1214fd855726d6f73423d2ee3a87e69d24d
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-11-30 00:39:28 -0300

    Improving production script, 'clone' if necessary

--- frontend/index.js
+++ frontend/index.js
@@ -3,10 +3,24 @@ const path = require('path');
 const app = express();
 const fs = require('fs');
 
-const exec = require('child_process').exec;
+const child_process = require('child_process');
 
-exec('rm -rf /home/exemvoox/exempliorevamp/build')
-exec('git clone https://github.com/marcuscastelo/exemplio-build-frontend build');
+function systemSync(cmd) {
+  try {
+    return child_process.execSync(cmd).toString();
+  } 
+  catch (error) {
+    error.status;  // Might be 127 in your example.
+    error.message; // Holds the message you typically want.
+    error.stderr;  // Holds the stderr output. Use `.toString()`.
+    error.stdout;  // Holds the stdout output. Use `.toString()`.
+  }
+};
+
+// console.log(systemSync('rm -rf ' + path.join(__dirname, 'build')));
+// console.log(systemSync('git clone https://github.com/marcuscastelo/exemplio-build-frontend build');
+console.log(systemSync("cd build; (git fetch && git status | grep 'up to date') && (git status -s | grep '.' && (echo 'Atualizando' && git fetch --all && git reset --hard origin/master) || echo 'Já atualizado') || (echo 'Atualizando' && git fetch --all && git reset --hard origin/master)"));
+
 
 const prefix = '';
 
@@ -17,7 +31,7 @@ app.get('/fallback', (req, res) => {
 })
 
 app.get('*/manifest.json', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'manifest.json'))
+	res.sendFile(path.join(__dirname, 'build', 'manifest.json'));
 });
 
 app.get(`${prefix}/favicon.png`, function(req, res) {
@@ -31,6 +45,10 @@ app.get(`${prefix}/*`, function(req, res) {
 
 app.get(prefix, function(req, res) {
   res.status(301).redirect(prefix + '/');
+});
+
+app.get("*", (req, res) => {
+    res.status(502).send("Unavailable resource");
 });
 
 app.listen(9000);
